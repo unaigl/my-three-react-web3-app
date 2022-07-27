6677eb6f41359b3702fa1cc696cc52ee759edd12
unaigl <unaiarki@gmail.com>
2022-07-27 19:39:49 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from '@react-three/fiber'
+import Box from './components/Box'
+import './App.css'
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
+
+export default App
