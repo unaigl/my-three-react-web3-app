812ad8d20f5ddd6bfd85bfed3f555724c1f572fc
unaigl <unaiarki@gmail.com>
2022-08-08 12:57:27 +0200

    structuring camera mode functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
-import { Suspense, useState, useRef } from 'react';
+import { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { CameraShake, OrbitControls, Sky, Html } from '@react-three/drei';
+import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon'
 
 import './App.css';
 import { Lights } from './lights/Lights';
-import CameraRig from './camera/CameraRig';
 import Stars from './stars/Stars';
 import GltfLoader from './gltf/GltfLoader';
 import ActivateSpawner from './geometry/ActivateSpawner';
+
+import CameraMode from './geometry/CameraMode';
 
 
 const App = () => {
@@ -16,17 +17,17 @@ const App = () => {
 	const [cameraMode, setCameraMode] = useState(true);
 	const [move, setmove] = useState(undefined);
 
-	const ref = useRef();
 
 	return (
 		<div className='row'>
 			<div className='col-lg-12 canvas'>
 				<Canvas
-					ref={ref}
 					camera={{
 						position: [0, 0, 0],
 						rotation: [0, 0, 0],
-					}}>
+					}}
+
+				>
 					{/* Details in "Stars" component */}
 					<Stars />
 					{/* Drei component, creates a sky background (does not give ilumination to canvas elements) */}
@@ -40,42 +41,15 @@ const App = () => {
 						{/* Our "GltfLoader" component takes a "position" and "cameraMove" setter to switch between two different camera controls*/}
 						<GltfLoader url={'chair/armchairYellow.gltf'} position={[0, -5, -15]} setCameraMode={() => {
 							setCameraMode(!cameraMode);
-							// todo blog, comentarios, ajustar rotacion
-							ref.current.camera.rotation.x = 0;
-							ref.current.camera.rotation.y = 0;
-							ref.current.camera.rotation.z = 0;
 						}} />
 					</Suspense>
 					{/* Several lights types are implemented in canvas */}
 					<Lights />
 
 					{/* switch between two different camera controls */}
-					{cameraMode ? <><OrbitControls
-						enableZoom={true}
-						enablePan={true}
-						enableRotate={true}
-					/>
-						{/* HTML is added only in Orbitcontrols, because creates a conflict using with "CameraShake" or "CameraRig" */}
-						<Html
-							as='h4'
-							position={[0, 0, -15]}
-							transform={true}
-						>
-							<h4 className="htmlcanvas" >Click to switch between two different camera controls</h4>
-						</Html>
-					</>
-						:
-						<>
-							{/* Drei component, creates a balanced movement in our "camera" position */}
-							<CameraShake
-								yawFrequency={0.1}
-								pitchFrequency={0.1}
-								rollFrequency={0.1}
-							/>
-							{/* Using "CameraRig", "camera" position reacts to our mouse movement */}
-							<CameraRig setmove={setmove} />
-						</>
-					}
+					{/* "CameraMode" components uses "useThree" (fiber-hook) (and must be inside the canvas) */}
+					<CameraMode cameraMode={cameraMode} setmove={setmove} />
+
 					{!cameraMode &&
 						< Physics >
 							<ActivateSpawner />
---
+++ src/components/geometry/CameraMode.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react'
+import { useThree } from '@react-three/fiber'
+import { CameraShake, OrbitControls, Html } from '@react-three/drei';
+
+import CameraRig from '../camera/CameraRig';
+
+const CameraMode = (props) => {
+
+    const { camera } = useThree()
+
+    useEffect(() => {
+        // accesing private variables of the camera
+        camera.rotation._x = 0;
+        camera.rotation._y = 0;
+        camera.rotation._z = 0;
+        console.log('camera', camera)
+        console.log('camera.rotation', camera.rotation)
+        // setting camera rotation when camera mode is changed 
+    }, [props.cameraMode])
+
+
+    return (
+        <>
+            {props.cameraMode ? <>< OrbitControls
+                enableZoom={true}
+                enablePan={true}
+                enableRotate={true}
+            />
+
+
+                {/* HTML is added only in Orbitcontrols, because creates a conflict using with "CameraShake" or "CameraRig" */}
+                < Html
+                    as='h4'
+                    position={[0, 0, -15]}
+                    transform={true}
+                >
+                    <h4 className="htmlcanvas" >Click to switch between two different camera controls</h4>
+                </Html >
+            </>
+                :
+                <>
+                    {/* Drei component, creates a balanced movement in our "camera" position */}
+                    <CameraShake
+                        yawFrequency={0.1}
+                        pitchFrequency={0.1}
+                        rollFrequency={0.1}
+                    />
+                    {/* Using "CameraRig", "camera" position reacts to our mouse movement */}
+                    <CameraRig setmove={props.setmove} />
+                </>
+            }
+        </>
+    )
+}
+
+export default CameraMode
